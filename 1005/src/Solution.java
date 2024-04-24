f4a059238bf7d1c1ee279c5ce6c19249360af823
zhengzhanglong <1196330394@qq.com>
2024-04-24 23:51:43 +0800

    4.24

--- 1005/src/Solution.java
+++ 1005/src/Solution.java
@@ -5,29 +5,23 @@ public class Solution {
 
         int point = 0;
         int sum = 0;
-        int minpos = Integer.MAX_VALUE;
 
         Arrays.sort(nums);
+
         for (int i = 0; i < k; i++) {
-            if (point == nums.length-1) {
-                if (nums[point] < 0)
-                    nums[point] = -nums[point];
-
-                minpos = Math.min(minpos, nums[point]);
-                if ((k - i)%2 == 1) {
-                    sum -= 2*minpos;
-                }
+            if (point >= nums.length) {
+                nums[nums.length - 1] = (k - i)%2 == 0 ? nums[nums.length - 1] : -nums[nums.length - 1]; ;
                 break;
-
             }
+
             if (nums[point] < 0) {
                 nums[point] = -nums[point];
-                minpos = Math.min(minpos, nums[point]);
                 point++;
             }else {
-                minpos = Math.min(minpos, nums[point]);
-                if ((k - i)%2 == 1) {
-                    sum -= 2*minpos;
+                if (point != 0 && nums[point-1] < nums[point]) {
+                    nums[point-1] = (k - i)%2 == 0 ? nums[point - 1] : -nums[point - 1];
+                }else {
+                    nums[point] = (k - i)%2 == 0 ? nums[point] : -nums[point];
                 }
                 break;
             }
---
+++ Code/src/_135/Solution.java
@@ -0,0 +1,29 @@
+package _135;
+
+public class Solution {
+    public int candy(int[] ratings) {
+        /**
+         分两个阶段
+         1、起点下标1 从左往右，只要 右边 比 左边 大，右边的糖果=左边 + 1
+         2、起点下标 ratings.length - 2 从右往左， 只要左边 比 右边 大，此时 左边的糖果应该 取本身的糖果数（符合比它左边大） 和 右边糖果数 + 1 二者的最大值，这样才符合 它比它左边的大，也比它右边大
+         */
+        int len = ratings.length;
+        int[] candyVec = new int[len];
+        candyVec[0] = 1;
+        for (int i = 1; i < len; i++) {
+            candyVec[i] = (ratings[i] > ratings[i - 1]) ? candyVec[i - 1] + 1 : 1;
+        }
+
+        for (int i = len - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candyVec[i] = Math.max(candyVec[i], candyVec[i + 1] + 1);
+            }
+        }
+
+        int ans = 0;
+        for (int num : candyVec) {
+            ans += num;
+        }
+        return ans;
+    }
+}
